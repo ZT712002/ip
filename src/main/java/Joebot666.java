0948fb6b66dc609999763a16c9a21fa6ab750614
zenden <zendentan71@gmail.com>
2025-08-23 13:40:35 +0800

    Added Joebot
    Start of Level-0 and project. Changed the text to the requirements of Level-0

---
+++ src/main/java/Joebot666.java
@@ -0,0 +1,7 @@
+public class Joebot666 {
+    public static void main(String[] args) {
+        System.out.println("Hello I'm JoeBot666");
+        System.out.println("What can I do for you today?");
+        System.out.println("Bye. See you next time!");
+    }
+}
