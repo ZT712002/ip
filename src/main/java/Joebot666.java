42738e085dff20d3d078f6b4b9f8d95637186222
zenden <zendentan71@gmail.com>
2025-08-27 12:16:26 +0800

    Added Extra Bot Prompt Flavors

--- src/main/java/Joebot666.java
+++ src/main/java/Joebot666.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class Joebot666 {
     public static void main(String[] args) {
         System.out.println("Hello I'm JoeBot666");
         System.out.println("What can I do for you today?");
+        Scanner in = new Scanner(System.in);
+        String userInput = in.nextLine();
+        while (!userInput.equals("bye")) {
+            System.out.println("*************************************");
+            System.out.println("Why do you say '" + userInput + "'?");
+            System.out.println("*************************************");
+            userInput = in.nextLine();
+        }
         System.out.println("Bye. See you next time!");
     }
 }
