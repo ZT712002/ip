a1f0103ac0d753cbdde53525dca7ca39ff9346dd
zenden <zendentan71@gmail.com>
2025-09-25 20:27:32 +0800

    Add Storage Class

--- src/main/java/Joebot666.java
+++ src/main/java/Joebot666.java
@@ -1,3 +1,4 @@
+import storage.Storage;
 import ui.Ui;
 import logic.TaskList;
 
@@ -7,31 +8,41 @@ import java.io.IOException;
 public class Joebot666 {
     private TaskList tasks;
     private Ui ui;
+    private Storage storage;
     public Joebot666() throws IOException {
         tasks = new TaskList();
         ui = new Ui();
+        storage = new Storage();
         greetUser();
     }
 
-    private void greetUser() throws IOException {
-        System.out.println("Hello I'm JoeBot666");
-        System.out.println("What can I do for you today?");
-        ui.initializeList(tasks);
-    }
-
-    public static void main(String[] args) throws IOException {
-        System.out.println("Hello I'm JoeBot666");
-        System.out.println("What can I do for you today?");
-        TaskList tasks = new TaskList();
-        Ui ui = new Ui();
-        ui.initializeList(tasks);
+    public void run() throws IOException {
         while (ui.isPasserActive()) {
             System.out.println("*************************************");
             ui.setUserInput();
             ui.processCommand(ui.getUserInput(), tasks);
         }
+        saveDataAndExit();
+    }
+
+    private void saveDataAndExit() {
+        try {
+            storage.saveData(tasks);
+        } catch (IOException e) {
+            System.out.println("Error saving data: " + e.getMessage());
+        }
         System.out.println("Bye. See you next time!");
         System.out.println("*************************************");
+    }
+
+    private void greetUser() throws IOException {
+        System.out.println("Hello I'm JoeBot666");
+        System.out.println("What can I do for you today?");
+        storage.initializeList(tasks);
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Joebot666().run();
     }
 
 }
